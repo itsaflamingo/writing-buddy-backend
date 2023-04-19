3c81221b6a2921f89048678d030a62c357d30c6d
Liz <elizabeth.pinero.r@gmail.com>
2023-04-19 13:13:12 -0400

    When user deleted, all child reference documents are deleted

--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
+const Project = require('../models/project');
+const Act = require('../models/act')
+const Chapter = require('../models/chapter')
+
 
 const UserSchema = new Schema({
     username: { type: String,  required: true  },
@@ -30,6 +34,35 @@ UserSchema.methods.isValidPassword = async function(password) {
     // return boolean returned by bcrypt.compare
     return compare;
 }
+
+// Middleware to remove child documents before deleting a user
+UserSchema.pre('findOneAndDelete', async function (next) {
+    const userId = this._conditions._id;
+    
+    // Find all projects associated with the user
+    const projects = await Project.find({ user: userId });
+  
+    // Get all project ids
+    const projectIds = projects.map(proj => proj._id);
+
+    for (const projectId of projectIds) {
+        // Find all acts associated with the project
+        const acts = await Act.find({ project: projectId });
+    
+        // Get all act ids
+        const actIds = acts.map(act => act._id);
+    
+        // Delete all chapters associated with the acts
+        await Chapter.deleteMany({ act: { $in: actIds } });
+    
+        // Delete all acts associated with the project
+        await Act.deleteMany({ project: projectId });
+      }
+    // Delete all acts associated with the project
+    await Project.deleteMany({ user: userId });
+    
+    next();
+  });
 
 module.exports = mongoose.model("User", UserSchema);
 
--- routes/hub.js
+++ routes/hub.js
@@ -4,8 +4,10 @@ var router = express.Router();
 const project_controller = require('../controllers/projectController');
 const act_controller = require('../controllers/actController');
 const chapter_controller = require('../controllers/chapterController');
+const user_controller = require('../controllers/userController');
 /* GET home page. */
 router.get('/', project_controller.projects_list );
+router.delete('/user/:id/delete', user_controller.delete_user);
 router.post('/user/:id/project/create', project_controller.create_project);
 // router.get('/project/:project_id/update', project_controller.get_update_project);
 // router.patch('/project/:project_id/update', project_controller.patch_update_project);
@@ -14,6 +16,7 @@ router.delete('/project/:project_id/delete', project_controller.delete_project);
 router.get('/project/:project_id/acts', act_controller.acts_list);
 router.post('/project/:project_id/act/create', act_controller.create_act);
 router.delete('/act/:act_id/delete', act_controller.delete_act);
+
 router.get('/act/:act_id/chapters', chapter_controller.chapters_list);
 router.post('/act/:act_id/chapter/create', chapter_controller.create_chapter);
 
