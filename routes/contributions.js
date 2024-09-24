f9ae0746cd6a1071e5f5b7272b6c54832bec66a6
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2024-09-24 18:55:07 -0400

    updated contributions router

--- routes/contributions.js
+++ routes/contributions.js
@@ -4,9 +4,6 @@ const router = express.Router();
 
 const contributions_controller = require("../controllers/contributionsController");
 
-router.get(
-  "/:user_id/contributions",
-  contributions_controller.user_contributions
-);
+router.get("/:user_id/", contributions_controller.user_contributions);
 
 module.exports = router;
