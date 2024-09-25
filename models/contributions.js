79c4013f15b79b19f80d4fd231272f7655077550
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2024-09-24 20:12:38 -0400

    added date_formatted virtual to contributions

--- models/contributions.js
+++ models/contributions.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const { Schema } = mongoose;
 
-const ContributionsSchema = new Schema({
-  date: { type: Date, default: Date.now, required: true },
-  numberOfContributions: { type: Number, required: true },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-    onDelete: "cascade",
+const ContributionsSchema = new Schema(
+  {
+    date: { type: Date, default: Date.now, required: true },
+    numberOfContributions: { type: Number, required: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+      onDelete: "cascade",
+    },
   },
-});
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
 
+ContributionsSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+});
 module.exports = mongoose.model("Contributions", ContributionsSchema);
